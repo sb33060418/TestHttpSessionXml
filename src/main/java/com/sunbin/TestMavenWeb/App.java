fa25582a28a72e1236591825e042b37462bb977e
sb33060418 <sb33060418@163.com>
2015-07-11 02:19:31 +0800

    test http session xml

---
+++ src/main/java/com/sunbin/TestMavenWeb/App.java
@@ -0,0 +1,13 @@
+package com.sunbin.TestMavenWeb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
